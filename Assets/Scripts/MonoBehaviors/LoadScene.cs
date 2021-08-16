371f4695fbb4284608c7a109dc2f85c99887b9a0
kksl <yw4146@nyu.edu>
2021-08-16 22:25:39 +0800

    Rough main menu made, build first before testing

---
+++ Assets/Scripts/MonoBehaviors/LoadScene.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public static LoadScene instance;
+    /*
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+    */
+    public void ChangToScene(string sceneToChangeTo)
+    {
+        SceneManager.LoadScene(sceneToChangeTo);
+    }
+
+}
